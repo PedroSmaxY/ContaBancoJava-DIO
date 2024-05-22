cb086e6ecc2bf7d4e58afb01dd954c92a98c71cc
PedroSmaxY <pedrohnovais@outlook.com>
2024-05-22 15:16:22 -0300

    Adicionar classe ContaTerminal para registro bancário

---
+++ src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String args[]) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("\nBem-Vindo ao Registro Bancário!\n");
+
+        System.out.print("Digite o seu nome completo: ");
+        String nomeCliente = entrada.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = entrada.nextInt();
+
+        System.out.print("Digite o número da agência: ");
+        String numeroAgencia = entrada.next();
+
+        System.out.print("Digite o número do seu saldo: ");
+        double saldo = entrada.nextDouble();
+
+        System.out.printf(
+                "\nOlá %s, obrigado por criar uma conta em nosso banco!\nSua agência é %s, conta %d e seu saldo %.2f já está disponível para saque\n",
+                nomeCliente, numeroAgencia, numeroConta, saldo);
+
+        entrada.close();
+    }
+}
